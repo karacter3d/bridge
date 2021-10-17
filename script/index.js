5895bf04a8ae5661e4de79d37c95acfe7f076f63
Shivansh Agrawal <shivansh11@outlook.com>
2021-10-17 21:04:25 +0530

    985 + behind the scene load

--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 var keysLeft = 0;
 var keys = [];
 var feed = 0;
+var divs = [];
+var isEnd = false;
 
 document.addEventListener('DOMContentLoaded', function(event) {
     
@@ -15,17 +17,17 @@ document.addEventListener('DOMContentLoaded', function(event) {
         index--;
     }
 
-    addDivs(); //Tries to add 20 div elements to .container div
+    loadnShow(); //Tries to add 20 div elements to .container div
 })
 
 //scroll event on window. if cond. = true when scrollbar reaches to the bottom minus some offset i.e. 2
 window.onscroll = function(ev) {
-    if (((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2) && keysLeft > 0) {
-        addDivs();
+    if (((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 2) && isEnd == false) {
+        showDivs();
     }
 };
 
-function addDivs () {
+function loadnShow () {
     var length = 20;
 
     if (keysLeft < 20) {
@@ -50,4 +52,49 @@ function addDivs () {
         keysLeft--;
         feed++;
     }
+
+    if(keysLeft > 0)
+        loadDivs();
+}
+
+function loadDivs () {
+
+    var length = 20;
+
+    if (keysLeft < 20) {
+        length = keysLeft;
+    }
+    
+    divs = [length];
+
+    for (i = 0; i < length; i++) {
+        (function () {
+            divs[i] = document.createElement("div");
+            var link = links[keys[feed]];
+            divs[i].className = 'item';
+            divs[i].style.backgroundImage = "url('image/" + keys[feed] + ".jpg')";
+            
+            divs[i].addEventListener('click', function() {
+                window.open(link, "_blank");
+            }, false);
+            
+            //document.getElementsByClassName('container')[0].appendChild(div[i]);
+        }()); // immediate invocation
+        keysLeft--;
+        feed++;
+    }
+    console.log("in load divs");
+}
+
+function showDivs () {
+    for (i = 0; i < divs.length; i++) {
+        (function () {
+            document.getElementsByClassName('container')[0].appendChild(divs[i]);
+        }()); // immediate invocation
+    }
+
+    if(keysLeft > 0)
+        loadDivs();
+    else
+        isEnd = true;
 }
--- script/links.js
+++ script/links.js
@@ -1,4 +1,5 @@
 var  links = {
+    985: "https://www.artstation.com/artwork/bKAGZd",
     984: "https://www.artstation.com/artwork/VgEa8n",
     983: "https://www.artstation.com/artwork/xJlEk2",
     982: "https://www.artstation.com/artwork/ZGNYKG",
