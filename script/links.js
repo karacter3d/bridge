e819d93ad13f7d127ef1ad51fa07e731f0e72c7f
Shivansh Agrawal <shivansh11@outlook.com>
2021-12-31 18:45:47 +0530

    1038

--- script/links.js
+++ script/links.js
@@ -1,4 +1,5 @@
 var  links = {
+    1038: "https://www.artstation.com/artwork/vJbyXv",
     1037: "https://www.artstation.com/artwork/wJrN2g",
     1036: "https://www.artstation.com/artwork/dOm8a3",
     1035: "https://www.artstation.com/artwork/Omvory",
