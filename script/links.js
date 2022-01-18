736ac280c3243b8081ee7bdd2240ec9aa12d0891
Shivansh Agrawal <shivansh11@outlook.com>
2022-01-18 20:25:15 +0530

    1056

--- script/links.js
+++ script/links.js
@@ -1,4 +1,5 @@
 var  links = {
+    1056: "https://www.artstation.com/yileina",
     1055: "https://www.artstation.com/ryoutaotsuka",
     1054: "https://www.artstation.com/michellechao3d",
     1053: "https://www.artstation.com/artwork/5PqJ",
