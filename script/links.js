d2f58aaded0ea6cb95957666beaf11b9355cbb24
Shivansh Agrawal <shivansh11@outlook.com>
2022-02-04 21:35:17 +0530

    1070

--- script/links.js
+++ script/links.js
@@ -1,4 +1,5 @@
 var  links = {
+    1070: "https://www.artstation.com/artwork/xYBnQm",
     1069: "https://www.artstation.com/artwork/qQ2XeN",
     1068: "https://www.artstation.com/artwork/qQoLyR",
     1067: "https://www.artstation.com/fenghao",
