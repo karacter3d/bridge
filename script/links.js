443071669eae6853b7ec07175d9e00333e7096e1
Shivansh <shivansh11@outlook.com>
2022-12-30 20:50:05 +0530

    1110

--- script/links.js
+++ script/links.js
@@ -1,4 +1,5 @@
 var  links = {
+    1110: "https://www.artstation.com/artwork/OmbP66",
     1109: "https://www.artstation.com/artwork/lR0dqG",
     1108: "https://www.artstation.com/artwork/d0X46x",
     1107: "https://www.artstation.com/crystal1992",
