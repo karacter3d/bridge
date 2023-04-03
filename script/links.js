69761f67e3fe0e01492af5ba322fb54fca98bc70
Shivansh <shivansh11@outlook.com>
2023-04-03 15:30:05 +0530

    1120

--- script/links.js
+++ script/links.js
@@ -1,4 +1,5 @@
 var  links = {
+    1120: "https://www.artstation.com/artwork/XgJa4w",
     1119: "https://www.artstation.com/artwork/YKkZaw",
     1118: "https://www.artstation.com/artwork/4XJ428",
     1117: "https://www.artstation.com/artwork/YKNr9V",
